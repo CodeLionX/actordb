064f2fec8c8d3bf27d30323361a089f851186ebe
CodeLionX <sebastian.schmidl@t-online.de>
2018-05-16 22:37:36 +0200

    added convinience methods for creating and lookingup dactors

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Dactor.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Dactor.scala
@@ -1,8 +1,16 @@
 package de.up.hpi.informationsystems.adbms
 
-import akka.actor.{Actor, ActorLogging}
-
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorRefFactory, ActorSelection, Props}
 import de.up.hpi.informationsystems.adbms.definition.Relation
+
+object Dactor {
+
+  def refFor(factory: ActorRefFactory, clazz: Class[_], id: Int): ActorRef =
+    factory.actorOf(Props(clazz, s"$id"), nameFor(clazz, s"$id"))
+
+  def nameFor(clazz: Class[_], name: String): String = s"${clazz.getSimpleName}-$name"
+
+}
 
 abstract class Dactor(name: String) extends Actor with ActorLogging {
 
@@ -13,6 +21,12 @@ abstract class Dactor(name: String) extends Actor with ActorLogging {
   protected val relations: Map[String, Relation]
 
   override def preStart(): Unit = log.info(s"${this.getClass.getSimpleName}($name) started")
-  
+
   override def postStop(): Unit = log.info(s"${this.getClass.getSimpleName}($name) stopped")
+
+  protected def dactorOf(clazz: Class[_], id: Int): ActorRef =
+    Dactor.refFor(context.system, clazz, id)
+
+  protected def dactorSelection(clazz: Class[_], id: Int): ActorSelection =
+    context.system.actorSelection(context.system / Dactor.nameFor(clazz, s"$id"))
 }
