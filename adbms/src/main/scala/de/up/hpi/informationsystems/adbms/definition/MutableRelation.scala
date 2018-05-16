0c46f635dd820647865b19e2d0f36256722e4b86
CodeLionX <sebastian.schmidl@t-online.de>
2018-05-16 16:52:42 +0200

    added delete-function to MutableRelations

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/definition/MutableRelation.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/definition/MutableRelation.scala
@@ -7,13 +7,14 @@ trait MutableRelation extends Relation {
   /**
     * Inserts a [[de.up.hpi.informationsystems.adbms.definition.Record]] into the relation
     * @param record to be inserted
+    * @return the record or an exception wrapped in a Try
     */
   def insert(record: Record): Try[Record]
 
   /**
-    *
-    * @param record
-    * @return
+    * Deletes the specified record from the relation and returns it.
+    * @param record to be deleted
+    * @return the record or an exception wrapped in a Try
     */
   def delete(record: Record): Try[Record]
 
