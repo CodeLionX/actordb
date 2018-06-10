ff670046ec85e6c48ab6105c4f0ed8bc2926d351
CodeLionX <sebastian.schmidl@t-online.de>
2018-06-10 22:09:13 +0200

    fixed prettyprinter (order of values and columns now match)

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Util.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Util.scala
@@ -5,9 +5,14 @@ import de.up.hpi.informationsystems.adbms.definition.{Record, UntypedColumnDef}
 object Util {
 
   def prettyTable(columns: Set[UntypedColumnDef], data: Seq[Record]): String = {
-    val header = columns.map { c => s"${c.name}[${c.tpe}]" }.mkString(" | ")
+    val orderedColumns = columns.toSeq
+    val header = orderedColumns.map { c => s"${c.name}[${c.tpe}]" }.mkString(" | ")
     val line = "-" * header.length
-    val content = data.map(_.values.mkString(" | ")).mkString("\n")
+    val content = data.map( record =>
+      orderedColumns.map( col =>
+        record(col)
+      ).mkString(" | ")
+    ).mkString("\n")
     header + "\n" + line + "\n" + content + "\n" + line + "\n"
   }
 
