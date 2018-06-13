70b0b2b89f268c455e0c5a9b738ce2fdfdf6b2b9
CodeLionX <sebastian.schmidl@t-online.de>
2018-06-13 14:25:03 +0200

    removed default message handling in Dactor

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Dactor.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/Dactor.scala
@@ -1,13 +1,11 @@
 package de.up.hpi.informationsystems.adbms
 
-import akka.actor.Status.{Failure, Success}
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorRefFactory, ActorSelection, ActorSystem, Props}
 import akka.util.Timeout
 import de.up.hpi.informationsystems.adbms.definition._
-import de.up.hpi.informationsystems.adbms.protocols.{DefaultMessagingProtocol, RequestResponseProtocol}
+import de.up.hpi.informationsystems.adbms.protocols.RequestResponseProtocol
 
 import scala.concurrent.Future
-import scala.util.Try
 
 object Dactor {
 
@@ -170,24 +168,4 @@ abstract class Dactor(id: Int) extends Actor with ActorLogging {
 
   override def postStop(): Unit = log.info(s"${this.getClass.getSimpleName}($id) stopped")
 
-  override def unhandled(message: Any): Unit = message match {
-    case DefaultMessagingProtocol.InsertIntoRelation(relationName, records) =>
-      handleGenericInsert(relationName, records) match {
-        case util.Success(_) => sender() ! Success
-        case util.Failure(e) => sender() ! Failure(e)
-      }
-    case _ => super.unhandled(message)
-  }
-
-
-  /**
-    * Inserts the specified records into the relation and returns the number of successfully inserted records.
-    *
-    * @param relationName name of the relation the records should be inserted to
-    * @param records      records to be inserted
-    * @return either number of successfully inserted records or a `Throwable` describing the failure
-    */
-  private def handleGenericInsert(relationName: String, records: Seq[Record]): Try[Int] = Try {
-    relationFromName(relationName).insertAll(records).map(_.count(_ => true))
-  }.flatten
 }
