5111d7add980845ec004aad555df3a6c1a05b07e
srfc <frederic.schneider@student.hpi.uni-potsdam.de>
2018-06-18 12:41:34 +0200

    WIP: Add Dactor.askDactor tests, TODO: failure cases

--- adbms/src/test/scala/de/up/hpi/informationsystems/adbms/DactorTest.scala
+++ adbms/src/test/scala/de/up/hpi/informationsystems/adbms/DactorTest.scala
@@ -5,17 +5,20 @@ import java.util.NoSuchElementException
 import akka.actor.{Actor, ActorNotFound, ActorRef, ActorSystem, InvalidActorNameException}
 import akka.testkit.{TestKit, TestProbe}
 import akka.util.Timeout
+import de.up.hpi.informationsystems.adbms.DactorTest.DactorWithRequestResponseBehaviour
+import de.up.hpi.informationsystems.adbms.DactorTest.DactorWithRequestResponseBehaviour.{RemindMe, SqrtMsg}
 import de.up.hpi.informationsystems.adbms.definition.ColumnTypeDefaults._
 import de.up.hpi.informationsystems.adbms.definition.{ColumnDef, RelationDef, UntypedColumnDef}
-import de.up.hpi.informationsystems.adbms.protocols.{DefaultMessageHandling, DefaultMessagingProtocol}
+import de.up.hpi.informationsystems.adbms.protocols.{DefaultMessageHandling, DefaultMessagingProtocol, RequestResponseProtocol}
 import de.up.hpi.informationsystems.adbms.record.ColumnCellMapping._
 import de.up.hpi.informationsystems.adbms.record.Record
-import de.up.hpi.informationsystems.adbms.relation.MutableRelation
+import de.up.hpi.informationsystems.adbms.relation.{MutableRelation, Relation}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.{Success, Try}
 
 object DactorTest {
   class TestDactor(id: Int) extends Dactor(id) {
@@ -53,6 +56,49 @@ object DactorTest {
   }
 
   class DactorWithRelation(id: Int) extends DactorWithRelation.DactorWithRelationBase(id) with DefaultMessageHandling
+
+  object DactorWithRequestResponseBehaviour {
+    object SqrtMsg {
+      case class Request(value: Int) extends RequestResponseProtocol.Request
+      case class Success(result: Relation) extends RequestResponseProtocol.Success
+      case class Failure(e: Throwable) extends RequestResponseProtocol.Failure
+    }
+
+    object RemindMe {
+      case class Request(reminder: String, time: Duration) extends RequestResponseProtocol.Request
+      case class Success(result: Relation) extends RequestResponseProtocol.Success
+      case class Failure(e: Throwable) extends RequestResponseProtocol.Failure
+    }
+
+    val sqrtColDef: ColumnDef[Double] = ColumnDef[Double]("sqrt")
+    val reminderColDef: ColumnDef[String] = ColumnDef[String]("reminder")
+  }
+
+  class DactorWithRequestResponseBehaviour(id: Int) extends Dactor(id) {
+    /**
+      * Returns a map of relation definition and corresponding relational store.
+      *
+      * @return map of relation definition and corresponding relational store
+      */
+    override protected val relations: Map[RelationDef, MutableRelation] = Map.empty
+
+    def calculateSqrt(value: Int): Try[Double] = Try{
+      if (value >= 0) Math.sqrt(value)
+      else throw new RuntimeException(s"Cannot calculate square root of negative value $value")
+    }
+
+    override def receive: Receive = {
+      case SqrtMsg.Request(value: Int) => calculateSqrt(value) match {
+        case scala.util.Success(sqrt) => sender() ! DactorWithRequestResponseBehaviour.SqrtMsg.Success(Relation(Seq(Record(Set(DactorWithRequestResponseBehaviour.sqrtColDef.untyped))(DactorWithRequestResponseBehaviour.sqrtColDef ~> sqrt).build())))
+        case scala.util.Failure(e) => sender() ! DactorWithRequestResponseBehaviour.SqrtMsg.Failure(e)
+      }
+
+      case RemindMe.Request(reminder, time) => {
+        Thread.sleep(time.toMillis)
+        sender() ! RemindMe.Success(Relation(Seq(Record(Set(DactorWithRequestResponseBehaviour.reminderColDef.untyped))(DactorWithRequestResponseBehaviour.reminderColDef ~> reminder).build())))
+      }
+    }
+  }
 }
 
 class DactorTest extends TestKit(ActorSystem("test-system"))
@@ -161,6 +207,29 @@ class DactorTest extends TestKit(ActorSystem("test-system"))
           val response = probe.expectMsgType[akka.actor.Status.Failure]
           response.cause shouldBe a[IncompatibleColumnDefinitionException]
         }
+      }
+    }
+
+    "supporting RequestResponseProtocol" should {
+
+      Dactor.dactorOf(system, classOf[DactorTest.DactorWithRequestResponseBehaviour], 1)
+
+      "answer an askDactor call with the corresponding Success message when receiving a request" in {
+        val fr = Dactor.askDactor(system, classOf[DactorTest.DactorWithRequestResponseBehaviour], Map(1 -> DactorTest.DactorWithRequestResponseBehaviour.SqrtMsg.Request(16)))
+        fr.records shouldEqual Success(Relation(Seq(
+          Record(Set(DactorTest.DactorWithRequestResponseBehaviour.sqrtColDef))(DactorWithRequestResponseBehaviour.sqrtColDef ~> 4).build()
+        )).records.get)
+      }
+
+      "answer an askDactor call with the corresponding Failure message when receiving a bad request" in {
+        val fr = Dactor.askDactor(system, classOf[DactorTest.DactorWithRequestResponseBehaviour], Map(1 -> DactorTest.DactorWithRequestResponseBehaviour.SqrtMsg.Request(-1)))
+        fr.records shouldEqual scala.util.Failure(new RuntimeException("Cannot calculate square root of negative value -1"))
+      }
+
+      "timeout an askDactor call for a request that takes too long to answer" in {
+        val fr = Dactor.askDactor(system, classOf[DactorTest.DactorWithRequestResponseBehaviour], Map(1 -> DactorTest.DactorWithRequestResponseBehaviour.RemindMe.Request("Wake up!", 2 seconds)))
+        // FIXME should time out
+        fr.records shouldEqual scala.util.Failure(new RuntimeException("Cannot calculate square root of negative value -1"))
       }
     }
   }
