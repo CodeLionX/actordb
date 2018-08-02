e4e078b9be1e2e3e95629d8ca2f9c6181a6dad44
srfc <frederic.schneider@student.hpi.uni-potsdam.de>
2018-08-02 11:32:57 +0200

    Change RequestResponseProtocol to use types

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/protocols/RequestResponseProtocol.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/protocols/RequestResponseProtocol.scala
@@ -4,28 +4,35 @@ import de.up.hpi.informationsystems.adbms.relation.Relation
 
 object RequestResponseProtocol {
 
+  /**
+    *
+    */
+  trait Message
+
   /** A Request to a Dactor for which you expect a Response.
     *
     * Each Dactor that can reply to queries subclasses these traits with their own message types so one can match on
     * expected message types
     */
-  trait Request
+  trait Request[+T <: Message]
 
-  sealed trait Response
+  sealed trait Response[+T <: Message]
 
   /** A successful Response from a Dactor in answer to a Request.
     *
+    * @tparam T [[RequestResponseProtocol.Message]] subtype to which this success response belongs
     * @see[[de.up.hpi.informationsystems.adbms.protocols.RequestResponseProtocol.Response]]
     */
-  trait Success extends Response {
+  trait Success[+T <: Message] extends Response[T] {
     def result: Relation
   }
 
   /** A failure Response that is used to inform about some failure during answering to a Request.
     *
+    * @tparam T [[RequestResponseProtocol.Message]] subtype to which this failure response belongs
     * @see[[de.up.hpi.informationsystems.adbms.protocols.RequestResponseProtocol.Response]]
     */
-  trait Failure extends Response {
+  trait Failure[+T <: Message] extends Response[T] {
     def e: Throwable
   }
 }
