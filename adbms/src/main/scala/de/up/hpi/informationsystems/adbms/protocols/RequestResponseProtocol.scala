28d85e3310041cfb3199ff4a3f77fed3800acebd
srfc <frederic.schneider@student.hpi.uni-potsdam.de>
2018-08-02 18:28:45 +0200

    Add extensive scaladoc for ReqResProtocol.Message

--- adbms/src/main/scala/de/up/hpi/informationsystems/adbms/protocols/RequestResponseProtocol.scala
+++ adbms/src/main/scala/de/up/hpi/informationsystems/adbms/protocols/RequestResponseProtocol.scala
@@ -6,6 +6,33 @@ object RequestResponseProtocol {
 
   /** Message type to be subclassed and used as [[Request]] and [[Response]] type information
     *
+    * Subclasses of this marker trait are used as type parameters for [[Request]], [[Success]], and [[Failure]]
+    * subclasses. The marker trait then allows for specific pattern matching on [[Request]] and [[Response]] subtypes.
+    *
+    * @example{{{
+    *           // Request and response message definitions in some Dactors companion object implementation:
+    *           object MyMessage {
+    *             sealed trait MyMessage extends Message
+    *             case class Request(someParam: SomeType) extends RequestResponseProtocol.Request[MyMessage]
+    *             case class Success(result: Relation) extends RequestResponseProtocol.Success[MyMessage]
+    *             case class Failure(e: Throwable) extends RequestResponseProtocol.Failure[MyMessage]
+    *           }
+    *
+    *           object OtherMessage {
+    *             sealed trait OtherMessage extends Message
+    *             case class Request(someParam: SomeType) extends RequestResponseProtocol.Request[OtherMessage]
+    *             case class Success(result: Relation) extends RequestResponseProtocol.Success[OtherMessage]
+    *             case class Failure(e: Throwable) extends RequestResponseProtocol.Failure[OtherMessage]
+    *           }
+    *
+    *           // Receive function of same Dactor reacting to [[Success]] messages of different types:
+    *           override def receive: Receive = {
+    *             case message: MyMessage.Success => // do something with this relation
+    *             case message: OtherMessage.Success => // do something else with that relation
+    *             case message: RequestResponseProtocol.Failure[_] => // match on the superclass to have common behavior
+    *               log.error(message.e.getMessage)
+    *           }
+    * }}}
     */
   trait Message
 
