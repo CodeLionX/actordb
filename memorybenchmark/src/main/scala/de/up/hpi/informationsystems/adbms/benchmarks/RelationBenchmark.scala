0f126f751975d115fff7a15ba434834efbba89c5
CodeLionX <sebastian.schmidl@t-online.de>
2018-09-23 14:15:01 +0200

    refactored relation benchmark

--- memorybenchmark/src/main/scala/de/up/hpi/informationsystems/adbms/benchmarks/RelationBenchmark.scala
+++ memorybenchmark/src/main/scala/de/up/hpi/informationsystems/adbms/benchmarks/RelationBenchmark.scala
@@ -1,13 +1,22 @@
 package de.up.hpi.informationsystems.adbms.benchmarks
 
-import java.io.File
+import java.io.{BufferedReader, File}
+import java.time.ZonedDateTime
 
-import de.up.hpi.informationsystems.adbms.csv.CSVParser.Implicits._
+import com.univocity.parsers.common.record.{Record => UVRecord}
+import com.univocity.parsers.csv.{CsvFormat, CsvParser, CsvParserSettings}
+import de.up.hpi.informationsystems.adbms.csv.CSVParser
+import de.up.hpi.informationsystems.adbms.record.Record
 import de.up.hpi.informationsystems.adbms.relation.{MutableRelation, Relation, RowRelation}
 import de.up.hpi.informationsystems.sampleapp.dactors._
 
+import scala.io.{Codec, Source}
+import scala.reflect.ClassTag
+import scala.util.Try
+import scala.util.control.NonFatal
+
 object RelationBenchmark extends App {
-  val dataDir = "/data/loadtest/data_100_mb"
+  val dataDir = "/data/relationtest/data-10000"
 
   val nameValMapping = Map(
     "cart_info" -> Cart.CartInfo,
@@ -21,20 +30,27 @@ object RelationBenchmark extends App {
   )
 
   def relationFromFile(f: File): Relation = {
-    var relationName = f.getCanonicalPath.split(File.separatorChar).last.split('.').head
-    try {
-      var tr: Relation = nameValMapping(relationName).readFromFile(f)
-      var rr: MutableRelation = RowRelation(nameValMapping(relationName))
-      rr.insertAll(tr.records.get)
-      relationName = null
-      tr = null
-      rr
-    } catch {
-      case e: Exception => {
-        println(f)
-        println(e)
-        throw e
-      }
+    val readRelation = (relationName: String, f: File) => {
+      println(s"Reading relation $relationName")
+      Try {
+        val rr: MutableRelation = RowRelation(nameValMapping(relationName))
+        Parser.readIntoMutableRelation(f, rr)
+        println("Finished")
+        rr
+
+      } recover {
+        case NonFatal(e) =>
+          println(s"Encountered $e during processing of file $f")
+          e.printStackTrace()
+          Relation.empty
+
+      } getOrElse
+        Relation.empty
+    }
+
+    f.getCanonicalPath.split(File.separatorChar).last.split('.').headOption match {
+      case Some(name) => readRelation(name, f)
+      case None => Relation.empty
     }
   }
 
@@ -57,4 +73,63 @@ object RelationBenchmark extends App {
 
   println(relations.last)
 
+}
+
+object Parser {
+  private implicit val fileCodec: Codec = Codec.UTF8
+
+  private val withHeader: Boolean = true
+  private val delimiter: Char = CSVParser.defaultDelimiter
+  private val lineSeparator: String = CSVParser.defaultLineSeparator
+
+  private val format: CsvFormat = {
+    val csvFormat = new CsvFormat()
+    csvFormat.setQuote('"')
+    csvFormat.setQuoteEscape('\\')
+    csvFormat.setCharToEscapeQuoteEscaping('\\')
+    csvFormat.setLineSeparator(lineSeparator)
+    csvFormat.setDelimiter(delimiter)
+    csvFormat
+  }
+  private def csvReader: CsvParser = {
+    val settings = new CsvParserSettings()
+    settings.setFormat(format)
+    settings.setHeaderExtractionEnabled(withHeader)
+    new CsvParser(settings)
+  }
+
+  private def readFile[T](file: File)(handler: BufferedReader => T): T = {
+    val source = Source.fromFile(file)
+    val reader = source.bufferedReader()
+    try {
+      handler(reader)
+    } finally {
+      source.close()
+    }
+  }
+
+  def readIntoMutableRelation(file: File, relation: MutableRelation): Unit = {
+    val columns = relation.columns
+    readFile(file)((in: BufferedReader) => {
+      val reader = csvReader
+      reader.beginParsing(in)
+
+      var record: UVRecord = reader.parseNextRecord()
+      var counter: Long = 1
+      while(record != null) {
+        val adbmsRecord: Record = Record.fromMap(
+          columns.toSeq.map {
+            case colDef if colDef.tpe == ClassTag.apply(classOf[ZonedDateTime]) => colDef -> ZonedDateTime.parse(record.getString(colDef.name))
+            case colDef => colDef -> record.getValue(colDef.name, colDef.default)
+          }.toMap
+        )
+        relation.insert(adbmsRecord)
+
+        if(counter % 1000000 == 0) println(s"processed $counter records")
+        record = reader.parseNextRecord()
+        counter += 1
+      }
+      reader.stopParsing()
+    })
+  }
 }
