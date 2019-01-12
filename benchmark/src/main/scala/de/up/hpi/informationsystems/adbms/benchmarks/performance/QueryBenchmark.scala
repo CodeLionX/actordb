e55820fd735d64e916b1722cd1bb91e89ed071fc
CodeLionX <sebastian.schmidl@t-online.de>
2019-01-12 20:27:51 +0100

    test for 30000 scan queries

--- benchmark/src/main/scala/de/up/hpi/informationsystems/adbms/benchmarks/performance/QueryBenchmark.scala
+++ benchmark/src/main/scala/de/up/hpi/informationsystems/adbms/benchmarks/performance/QueryBenchmark.scala
@@ -1,7 +1,10 @@
 package de.up.hpi.informationsystems.adbms.benchmarks.performance
 
+import java.io.FileWriter
+
 import akka.actor.{Actor, ActorIdentity, ActorLogging, ActorRef, ActorSystem, Cancellable, Identify, Props}
 import de.up.hpi.informationsystems.adbms.benchmarks.performance.BulkInsertBenchmark.{SystemInitializer => BISystemInitializer}
+import de.up.hpi.informationsystems.adbms.protocols.RequestResponseProtocol
 import de.up.hpi.informationsystems.adbms.protocols.RequestResponseProtocol.Response
 import de.up.hpi.informationsystems.adbms.relation.{Relation, RelationBinOps}
 import de.up.hpi.informationsystems.sampleapp.dactors.StoreSection.GetAvailableQuantityFor
@@ -58,16 +61,34 @@ object QueryBenchmark extends App {
 
       case Timeout =>
         println("start measuring queries")
-        val functor = context.actorOf(Props[ScanQueryFunctor])
-        functor ! Start(storeSections)
-        context.become(waitForQueryResult())
+        startQueries(storeSections, 30000)
     }
 
-    def waitForQueryResult(): Receive = {
-      case rel: Relation =>
-        println("Received results")
-        println(rel)
-        handleShutdown()
+    def waitForQueryResult(pendingACKs: Map[ActorRef, Long], finishedTimes: Seq[Long], startTime: Long): Receive = {
+      case _: Relation =>
+        val runtime = System.nanoTime() - pendingACKs(sender)
+        val newFinishedTimes = finishedTimes :+ runtime
+        val newPendingACKs = pendingACKs.filterNot( t => t._1 == sender() )
+
+        if(newPendingACKs.isEmpty) {
+          val endTime = System.nanoTime()
+
+          val filename: String = "results.txt"
+          println("========== elapsed time (scan query) ==========")
+          println(s"elapsed nanos: ${endTime - startTime}ns (${(endTime - startTime) / 1000000000}s)")
+          println(s"average time per query: ${finishedTimes.sum/finishedTimes.count(_ => true)}ns")
+          println(s"Collected ${newFinishedTimes.size} measurements")
+
+          val resultString = newFinishedTimes.mkString("", "\n", "")
+          val fw = new FileWriter(filename, false)
+          fw.write(resultString)
+          fw.close()
+
+          println(s"Results written to $filename")
+          handleShutdown()
+        } else {
+          context.become(waitForQueryResult(newPendingACKs, newFinishedTimes, startTime))
+        }
     }
 
     def resolveStoreSectionIdentities(): Unit = {
@@ -79,32 +100,45 @@ object QueryBenchmark extends App {
       context.system.scheduler.scheduleOnce(3 seconds, self, Timeout)
       context.become(waitForIdentities(Seq.empty) orElse commonBehavior)
     }
+
+    def startQueries(storeSections: Seq[ActorRef], n: Int = 100000): Unit = {
+      val startTime = System.nanoTime()
+      val pending = (0 until n).map( _ => {
+        val begin = System.nanoTime()
+        val functor = context.actorOf(Props[ScanQueryFunctor[GetAvailableQuantityFor]])
+        functor ! Start(storeSections, GetAvailableQuantityFor.Request(115))
+        functor -> begin
+      }).toMap
+      context.become(waitForQueryResult(pending, Seq.empty, startTime))
+    }
   }
 
-  case class Start(storeSections: Seq[ActorRef])
+  case class Start(dactors: Seq[ActorRef], message: RequestResponseProtocol.Request[RequestResponseProtocol.Message])
 
-  class ScanQueryFunctor extends Actor with ActorLogging {
+  class ScanQueryFunctor[T <: RequestResponseProtocol.Message] extends Actor with ActorLogging {
 
     override def receive: Receive = sendRequest
 
     def sendRequest: Receive = {
-      case Start(storeSections) =>
-        log.info(s"Started scan query functor with ${storeSections.length} target actors")
-        storeSections.foreach(_ ! GetAvailableQuantityFor.Request(115))
-        context.become(waitForResults(sender, storeSections, Seq.empty))
+      case Start(dactors, message) =>
+        log.info(s"Started scan query functor with ${dactors.length} target dactors")
+        dactors.foreach(_ ! message)
+        context.become(waitForResults(sender, dactors, Seq.empty))
     }
 
-    def waitForResults(receiver: ActorRef, pending: Seq[ActorRef], results: Seq[Response[GetAvailableQuantityFor]]): Receive = {
-      case result: Response[GetAvailableQuantityFor] =>
+    def waitForResults(receiver: ActorRef, pending: Seq[ActorRef], results: Seq[Response[T]]): Receive = {
+      case result: Response[T] =>
         val newPending = pending.filterNot(_ == sender)
         if (newPending.isEmpty) {
           log.info("Received all results of scan query, sending result to receiver")
-          val concatenatedResult = results
+          val results2 = results
             .flatMap {
-              case GetAvailableQuantityFor.Success(res) => Some(res)
-              case GetAvailableQuantityFor.Failure(_) => None
+              case res: RequestResponseProtocol.Success[T] => Some(res.result)
+              case _: RequestResponseProtocol.Failure[T] => None
             }
-            .reduce(RelationBinOps.union)
+          val concatenatedResult =
+            if(results2.nonEmpty) results2.reduce(RelationBinOps.union)
+            else Relation.empty
           receiver ! concatenatedResult
           context.stop(self)
 
